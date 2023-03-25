8fefb734567786d3bb653dfcfb4f881a234ab59d
KiranGehlot <85874513+KiranGehlot@users.noreply.github.com>
2023-03-25 17:05:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload() {
+
+}
+
+
+function setup() {
+    canvas=createCanvas(200 , 200)
+    canvas.center();
+}
+
+function draw()  {
+
+}
+
+
+function take_snapshot()  {
+    save("pic.png")
+    
+}
